98f3dd1326e03888e84bbb076f305009c8590f2a
haru067 <haru.j2@gmail.com>
2020-07-16 23:49:52 +0900

    Add menu detail UI

--- lib/screens/shop_menu/widgets/menu_detail_screen.dart
+++ lib/screens/shop_menu/widgets/menu_detail_screen.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:ramen/screens/shop_menu/entities/menu.dart';
 
 class MenuDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Menu menu = ModalRoute.of(context).settings.arguments;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Menu"),
-      ),
-      body: Text("sss"),
+        appBar: AppBar(
+          title: Text(menu.title),
+        ),
+        body: _buildBody(context, menu));
+  }
+
+  Widget _buildBody(BuildContext context, Menu menu) {
+    return ListView(
+      children: <Widget>[
+        _buildHeader(menu),
+        Container(
+          margin: EdgeInsets.only(right: 16, left: 16, top: 16, bottom: 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(menu.title, style: Theme.of(context).textTheme.headline6),
+              Padding(
+                padding: EdgeInsets.only(top: 8, bottom: 8),
+                child: Text("${menu.price}円",
+                    style: Theme.of(context).textTheme.subtitle1),
+              ),
+              Text(menu.subTitle, style: Theme.of(context).textTheme.bodyText1),
+              IconButton(icon: Icon(Icons.favorite_border), onPressed: null)
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildHeader(Menu menu) {
+    if (menu == null) return Container();
+
+    return Container(
+      height: 320,
+      color: Colors.blueGrey,
+      alignment: Alignment.center,
+      child: Container(
+          color: Colors.blueGrey,
+          constraints: BoxConstraints.expand(),
+          child: Image.network("${menu.imageUrl}", fit: BoxFit.cover)),
     );
   }
 }
