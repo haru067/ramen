86a37621941a7736f20791d257ac0aa597ba1956
haru067 <haru.j2@gmail.com>
2020-07-18 01:15:05 +0900

    Fix notice UI

--- lib/screens/home/widgets/notice_list.dart
+++ lib/screens/home/widgets/notice_list.dart
@@ -14,7 +14,7 @@ class NoticeList extends StatelessWidget {
           return Container(
             padding: EdgeInsets.only(left: 16.0, right: 16.0),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: _getNoticeWidgets(context, snapshot),
             ),
           );
@@ -38,19 +38,29 @@ class NoticeList extends StatelessWidget {
 
   Widget _getNoticeWidget(BuildContext context, Notice notice) {
     return Container(
-        decoration: BoxDecoration(
-          border: Border(
-            bottom: BorderSide(color: Colors.black12),
-          ),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: Colors.black12),
         ),
-        padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(notice.title, style: Theme.of(context).textTheme.subtitle1),
-            Text(notice.description,
-                style: Theme.of(context).textTheme.bodyText2),
-          ],
-        ));
+      ),
+      child:
+          Row(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+        Padding(
+            padding: EdgeInsets.only(right: 24, top: 16),
+            child: Icon(
+              Icons.info,
+              size: 24,
+              color: Colors.blue,
+            )),
+        Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+          Container(height: 14),
+          Text(notice.title, style: Theme.of(context).textTheme.subtitle1),
+          Container(height: 4),
+          Text(notice.description,
+              style: Theme.of(context).textTheme.bodyText1),
+          Container(height: 16),
+        ]),
+      ]),
+    );
   }
 }
